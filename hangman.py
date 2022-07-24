8471d1c429416a765db77adaa774e566905ec583
Ewa Bittner <bittner.ewa@gmail.com>
2022-07-24 17:06:04 +0200

    Added removal of lives for mistakes in the hangman game

--- hangman.py
+++ hangman.py
@@ -15,32 +15,39 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #what the user has guessed
 
+    lives = 7
+
     #getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         #letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         #what current word is (ie W - RD)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
-
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1 #takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
 
         elif user_letter in used_letters:
-            print('You have already used that character. Please try again.')
+            print('\nYou have already used that character. Guess another letter.')
 
         else:
-            print('Invalid character. Please try again.')
+            print('\nInvalid character. Please try again.')
 
-    #gets here when len(word_letters) == 0
+    #gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
+        print('You guessed the word', word, '!!')
 
 hangman()
-
-user_input = input('Type something: ')
-print(user_input)
