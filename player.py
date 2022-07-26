de722d2ae5d392287f50da0f236ce11ddedf024a
Ewa Bittner <bittner.ewa@gmail.com>
2022-07-26 23:36:14 +0200

    Code finished, some bugs left - exiting too fast

--- player.py
+++ player.py
@@ -1,27 +1,42 @@
-from lib2to3.pytree import LeafPattern
 import math
 import random
 
 class Player:
-    def __init__(self, leter):
+    def __init__(self, letter):
         # letter is x or o
-        self.letter = LeafPattern
+        self.letter = letter
 
     # we want all players to get their next more given a game
     def get_move(self, game):
         pass
 
 
-    class RandomComputerPlayer(Player):
-        def __init__(self, letter):
-            super().__init__(letter)
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
 
-        def get_move(self, game):
-            pass
+    def get_move(self, game):
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
 
-    class HumanPlayer(Player):
-        def __init__(self, letter):
-            super().__init__(leter)
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
 
-        def get_move(self, game):
-            pass
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            squuare = input(self.letter + '\'s turn.Input move (0-8): ')
+            # we're going to check that is a correct value by trying to cast 
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(squuare)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True # if these are sucessful, than yay!
+            except ValueError:
+                print('Invalid square. Try again. ')
+        return val
